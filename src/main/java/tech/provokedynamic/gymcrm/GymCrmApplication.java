de885ecc4e2832c6b8e3a95288a70847e4078ba0
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-05-03 22:13:07 +0400

    feat: add logback configuration with dev and prod profiles

--- src/main/java/tech/provokedynamic/gymcrm/GymCrmApplication.java
+++ src/main/java/tech/provokedynamic/gymcrm/GymCrmApplication.java
@@ -1,11 +1,33 @@
 package tech.provokedynamic.gymcrm;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
+import org.springframework.boot.autoconfigure.task.TaskSchedulingAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootConfiguration(proxyBeanMethods = false)
+@EnableAutoConfiguration(exclude = {
+        JmxAutoConfiguration.class,
+        TaskExecutionAutoConfiguration.class,
+        TaskSchedulingAutoConfiguration.class,
+})
+@ComponentScan(basePackages = {
+
+})
 public class GymCrmApplication {
     static void main(String[] args) {
-        SpringApplication.run(GymCrmApplication.class, args);
+        new SpringApplicationBuilder()
+                .sources(GymCrmApplication.class)
+                .web(WebApplicationType.NONE)
+                .bannerMode(Banner.Mode.OFF)
+                .logStartupInfo(false)
+                .headless(false)
+                .build(args)
+                .run();
     }
 }
