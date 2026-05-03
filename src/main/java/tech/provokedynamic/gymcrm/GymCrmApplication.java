801b5dcd97ba483020833049a069f86737fe4436
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-05-03 22:02:37 +0400

    feat: initial project setup for Gym CRM Spring Core module

---
+++ src/main/java/tech/provokedynamic/gymcrm/GymCrmApplication.java
@@ -0,0 +1,11 @@
+package tech.provokedynamic.gymcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymCrmApplication {
+    static void main(String[] args) {
+        SpringApplication.run(GymCrmApplication.class, args);
+    }
+}
