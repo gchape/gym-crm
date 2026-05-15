430481324a3cd27be05651188699b4c33c2828f3
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-05-15 20:48:21 +0400

    feat(entity): implement Training entity with FK relations to Trainee, Trainer and TrainingType

--- src/main/java/tech/provokedynamic/gymcrm/entity/Training.java
+++ src/main/java/tech/provokedynamic/gymcrm/entity/Training.java
@@ -1,4 +1,43 @@
 package tech.provokedynamic.gymcrm.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Training {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false, insertable = false)
+    private Long id;
+
+    @NotNull
+    private String trainingName;
+
+    @NotNull
+    private LocalDate trainingDate;
+
+    @NotNull
+    private Integer trainingDuration;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "trainee_id")
+    private Trainee trainee;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "trainer_id")
+    private Trainer trainer;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "training_type_id")
+    private TrainingType trainingType;
 }
