a4699f031372b00c26a6c96ff93f872eeaa1a62f
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-05-15 20:48:21 +0400

    feat(entity): implement Trainee with ManyToMany relation to Trainer

--- src/main/java/tech/provokedynamic/gymcrm/entity/Trainee.java
+++ src/main/java/tech/provokedynamic/gymcrm/entity/Trainee.java
@@ -1,4 +1,59 @@
 package tech.provokedynamic.gymcrm.entity;
 
-public class Trainee {
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.proxy.HibernateProxy;
+import tech.provokedynamic.gymcrm.model.Address;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Getter
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DiscriminatorValue("trainee")
+public class Trainee extends User {
+    @ManyToMany
+    @JoinTable(
+            name = "trainee_trainer",
+            joinColumns = @JoinColumn(name = "trainee_id"),
+            inverseJoinColumns = @JoinColumn(name = "trainer_id")
+    )
+    private final Set<Trainer> trainers = new HashSet<>();
+
+    @Nullable
+    @Column(name = "dob")
+    private LocalDate dateOfBirth;
+
+    @Nullable
+    @Embedded
+    private Address address;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy hp
+                ? hp.getHibernateLazyInitializer().getPersistentClass()
+                : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy hp
+                ? hp.getHibernateLazyInitializer().getPersistentClass()
+                : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Trainee trainee = (Trainee) o;
+        return Objects.equals(getId(), trainee.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy hp
+                ? hp.getHibernateLazyInitializer().getPersistentClass().hashCode()
+                : getClass().hashCode();
+    }
 }
