5a6348dbfa81f75fbc9d315e702151e0e37edf0f
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-05-15 14:46:20 +0400

    feat: define User entity with soft delete, sequence ID, and validation constraints

--- src/main/java/tech/provokedynamic/gymcrm/entity/User.java
+++ src/main/java/tech/provokedynamic/gymcrm/entity/User.java
@@ -1,16 +1,45 @@
 package tech.provokedynamic.gymcrm.entity;
-@jakarta.persistence.Entity
-@jakarta.persistence.Table(name = "user")
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import static jakarta.persistence.GenerationType.SEQUENCE;
+import static lombok.AccessLevel.PROTECTED;
+import static org.hibernate.annotations.SoftDeleteType.ACTIVE;
+
+@Entity
+@Table(name = "\"user\"")
+@lombok.Getter
+@lombok.NoArgsConstructor(access = PROTECTED)
+@org.hibernate.annotations.SoftDelete(strategy = ACTIVE, columnName = "is_active")
 public class User {
-@jakarta.persistence.Id
-@jakarta.persistence.GeneratedValue(strategy = jakarta.persistence.GenerationType.SEQUENCE)
-@jakarta.persistence.Column(name = "id", nullable = false)
-private java.lang.Long id;
+    @Id
+    @GeneratedValue(strategy = SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
 
-public java.lang.Long getId() {
-  return id;
-}public void setId(java.lang.Long id) {
-  this.id = id;
-}
+    @NotNull
+    @Column(length = 50)
+    private String firstName;
 
+    @NotNull
+    @Column(length = 50)
+    private String lastName;
+
+    @NotNull
+    @Column(length = 128, unique = true)
+    private String username;
+
+    @NotNull
+    @Column(length = 10)
+    private String password;
+
+    public static User of(String firstName, String lastName, String username, String password) {
+        var user = new User();
+        user.firstName = firstName;
+        user.lastName = lastName;
+        user.username = username;
+        user.password = password;
+        return user;
+    }
 }
