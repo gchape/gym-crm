9c71f19f9e8b74f1dfe92662fdb33ef4cdd4c6f7
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-05-15 20:48:21 +0400

    feat(entity): implement Trainer with ManyToOne relation to TrainingType

--- src/main/java/tech/provokedynamic/gymcrm/entity/Trainer.java
+++ src/main/java/tech/provokedynamic/gymcrm/entity/Trainer.java
@@ -1,4 +1,49 @@
 package tech.provokedynamic.gymcrm.entity;
 
-public class Trainer {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Getter
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DiscriminatorValue("trainer")
+public class Trainer extends User {
+    @ManyToMany(mappedBy = "trainers")
+    private final Set<Trainee> trainees = new HashSet<>();
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "training_type_id")
+    private TrainingType specialization;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy hp
+                ? hp.getHibernateLazyInitializer().getPersistentClass()
+                : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy hp
+                ? hp.getHibernateLazyInitializer().getPersistentClass()
+                : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Trainer trainer = (Trainer) o;
+        return Objects.equals(getId(), trainer.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy hp
+                ? hp.getHibernateLazyInitializer().getPersistentClass().hashCode()
+                : getClass().hashCode();
+    }
 }
