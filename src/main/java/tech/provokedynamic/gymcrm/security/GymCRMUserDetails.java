10dea392f506a3641769131c0300286c7edd28b9
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-06-15 10:18:41 +0400

    feat(security): implement UserDetails and UserDetailsService

--- src/main/java/tech/provokedynamic/gymcrm/security/GymCRMUserDetails.java
+++ src/main/java/tech/provokedynamic/gymcrm/security/GymCRMUserDetails.java
@@ -1,4 +1,51 @@
 package tech.provokedynamic.gymcrm.security;
 
-public class GymCRMUserDetails {
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.NullUnmarked;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.CredentialsContainer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import tech.provokedynamic.gymcrm.entity.User;
+
+import java.util.Collection;
+import java.util.List;
+
+@NullMarked
+public class GymCRMUserDetails implements UserDetails, CredentialsContainer {
+
+    private final String username;
+    private final List<SimpleGrantedAuthority> authorities;
+
+    private @Nullable String password;
+
+    public GymCRMUserDetails(User user) {
+        username = user.getUsername();
+        password = user.getPassword();
+        authorities = user.getRoles().stream()
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    @NullUnmarked
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public void eraseCredentials() {
+        password = null;
+    }
 }
