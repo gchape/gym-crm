d6968a63b3e648aa75361edea0f751a315ff14fd
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-06-15 10:18:33 +0400

    chore(config): move CredentialGenerator bean, add security dep, set ddl-auto to validate

--- src/main/java/tech/provokedynamic/gymcrm/GymCrmApplication.java
+++ src/main/java/tech/provokedynamic/gymcrm/GymCrmApplication.java
@@ -3,6 +3,10 @@ package tech.provokedynamic.gymcrm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.Bean;
+import tech.provokedynamic.gymcrm.repository.UserRepository;
+import tech.provokedynamic.gymcrm.util.CredentialGenerator;
+import tech.provokedynamic.gymcrm.util.DBCredentialGenerator;
 
 @SpringBootApplication(scanBasePackages = {
         "tech.provokedynamic.gymcrm.aspect",
@@ -10,11 +14,18 @@ import org.springframework.boot.persistence.autoconfigure.EntityScan;
         "tech.provokedynamic.gymcrm.service",
         "tech.provokedynamic.gymcrm.controller",
         "tech.provokedynamic.gymcrm.config",
+        "tech.provokedynamic.gymcrm.security",
+        "tech.provokedynamic.gymcrm.repository",
 }, proxyBeanMethods = false)
 @EntityScan("tech.provokedynamic.gymcrm.entity")
 public class GymCrmApplication {
 
     static void main(String[] args) {
         SpringApplication.run(GymCrmApplication.class, args);
+    }
+
+    @Bean
+    public CredentialGenerator credentialGenerator(UserRepository userRepository) {
+        return new DBCredentialGenerator(userRepository);
     }
 }
